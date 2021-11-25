ba3fceea1879d5cc8b6cad3402b3623f07c595bb
Vladimir Siritsa <vladimir.siritsa@bitfocus.com>
2021-11-25 21:10:42 +0200

    added autosuggestion plugin added syntax-highlight plugin plugins now installing as submodules

--- install.sh
+++ install.sh
@@ -4,18 +4,26 @@ set -euo pipefail
 
 PWD=$(pwd)
 INSTALL_DIR="$HOME/.oh-my-zsh"
+CONFIG_FILES=(".zshrc.local" ".tmux.conf" ".zsh_plugins" ".zsh_aliases")
+
+git submodule init
+git submodule update
 
 if [[ -d "$INSTALL_DIR" ]]; then
   mv "$INSTALL_DIR" "$INSTALL_DIR.bak"
 fi
+
+for fileName in "${CONFIG_FILES[@]}"; do
+  if [[ -r "$HOME/$fileName" ]]; then
+    mv "$HOME/$fileName" "$HOME/$fileName.bak"
+  fi
+done
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
 
 if [[ -e "$HOME/.zshrc" ]]; then
   mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
 fi
-
-git clone https://github.com/popstas/zsh-command-time.git "$PWD/custom/plugins/command-time"
 
 for PLUGIN_PATH in "$PWD"/custom/plugins/*; do
   PLUGIN_NAME="$(basename "$PLUGIN_PATH")"
@@ -27,15 +35,20 @@ for THEME_PATH in "$PWD"/custom/themes/*; do
   ln -s "$THEME_PATH" "$INSTALL_DIR/custom/themes/$THEME_NAME"
 done
 
-ln -s "$PWD/.zshrc.local" "$HOME/.zshrc.local"
-ln -s "$PWD/.tmux.conf" "$HOME/.tmux.conf"
+for fileName in "${CONFIG_FILES[@]}"; do
+  ln -s "$PWD/$fileName" "$HOME/$fileName"
+  if [[ -r "$HOME/$fileName.bak" ]]; then
+    echo ""
+    echo "$HOME/$fileName.bak >>>> $HOME/$fileName"
+    diff "$HOME/$fileName.bak" "$HOME/$fileName"
+    echo "====================="
+  fi
+done
 
-cp "$PWD/.zsh_plugins" "$HOME/.zsh_plugins"
-cp "$PWD/.zsh_aliases" "$HOME/.zsh_aliases"
 cp "$INSTALL_DIR/templates/zshrc.zsh-template" "$HOME/.zshrc"
 
 sed -E "s/^source\ \\\$ZSH\/oh-my-zsh.sh$//g" -i "$HOME/.zshrc"
 
-echo -e "[[ -r \"\$HOME/.zshrc.local\" ]] && source \"\$HOME/.zshrc.local\"" >> "$HOME/.zshrc"
+echo -e "[[ -r \"\$HOME/.zshrc.local\" ]] && source \"\$HOME/.zshrc.local\"" >>"$HOME/.zshrc"
 
 chsh -s /bin/zsh "$(whoami)"
