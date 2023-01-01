551cc0f40a066d3758864d9216613bc1e3d8bba7
Vladimir Siritsa <v_siritsa@quartsoft.com>
2023-01-01 13:48:31 +0200

    force creating symlinks added

--- update.sh
+++ update.sh
@@ -21,7 +21,7 @@ LOCAL_BIN_PATH="$HOME/.local/bin"
 for TARGET_PATH in "$PWD"/custom/plugins/*; do
   TARGET_NAME="$(basename "$TARGET_PATH")"
   LINK="$INSTALL_DIR/custom/plugins/$TARGET_NAME"
-  if [[ ! -r $LINK ]]; then
+  if [[ ! -L $LINK ]]; then
     ln -sf "$TARGET_PATH" "$LINK"
   fi
 done
@@ -29,7 +29,7 @@ done
 for TARGET_PATH in "$PWD"/custom/themes/*; do
   TARGET_NAME="$(basename "$TARGET_PATH")"
   LINK="$INSTALL_DIR/custom/themes/$TARGET_NAME"
-  if [[ ! -r $LINK ]]; then
+  if [[ ! -L $LINK ]]; then
     ln -sf "$TARGET_PATH" "$LINK"
   fi
 done
@@ -38,7 +38,7 @@ mkdir -p "$LOCAL_BIN_PATH"
 for TARGET_PATH in "$PWD"/bin/*; do
   TARGET_NAME="$(basename "$TARGET_PATH")"
   LINK="$LOCAL_BIN_PATH/$TARGET_NAME"
-  if [[ ! -r $LINK ]]; then
+  if [[ ! -L $LINK ]]; then
     ln -sf "$TARGET_PATH" "$LINK"
   fi
 done
@@ -46,8 +46,8 @@ done
 for fileName in "${CONFIG_FILES[@]}"; do
   TARGET_PATH="$PWD/$fileName"
   LINK="$HOME/$fileName"
-  mkdir -p $(dirname "$LINK")
-  if [[ ! -r $LINK ]]; then
+  mkdir -p "$(dirname "$LINK")"
+  if [[ ! -L $LINK ]]; then
     ln -s "$TARGET_PATH" "$LINK"
   fi
 done
